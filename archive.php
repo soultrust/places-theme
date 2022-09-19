cac2e8681668962121bae57573f43e4ceac60ba2
Tomo Suzuki <tomo@soultrust.com>
2022-09-18 20:21:53 -0400

    Make category and tag archive pages work

--- archive.php
+++ archive.php
@@ -4,10 +4,22 @@ get_header();
 <h2 class="archive-title"><?php the_archive_title(); ?></h2>
 <ul class="links-list">
 <?php
-  $places = new WP_Query(array(
+  $tag_args = array(
     'post_type' => 'place',
     'tag' => single_tag_title('', false)
-  ));
+  );
+  $category_args = array(
+    'post_type' => 'place',
+    'category' => single_cat_title('', false)
+  );
+
+  if (is_tag()) {
+    $places = new WP_Query($tag_args);
+  }
+  if (is_category()) {
+    $places = new WP_Query($category_args);
+  }
+  
   while ($places->have_posts()) {
     $places->the_post(); ?>
     <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
